b1b1d1938c792d5dd9c99834d17e42043c96c502
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-09-09 07:50:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# apm-node-auto-lambda
+
+This is included in the appoptics-apm layer for the nodejs runtime
+environments. It is not intended to be used on its own.
---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const apm = process.env.AO_TEST_LAMBDA_APM || 'appoptics-apm';
+const runtime = process.env.AO_TEST_LAMBDA_RUNTIME || '/var/runtime/UserFunction';
+const ao = require(apm);
+
+const taskRoot = process.env.LAMBDA_TASK_ROOT || '';
+const handler = process.env.APPOPTICS_LAMBDA_HANDLER || '';
+
+// use the lambda runtime's loading logic
+let load;
+let userHandler;
+try {
+  ({load} = require(runtime));
+  userHandler = load(taskRoot, handler);
+} catch (e) {
+  ao.loggers.error(`failed to load ${handler}`, e);
+}
+
+if (ao.cfg.enabled) {
+  userHandler = ao.wrapLambdaHandler(userHandler);
+}
+
+module.exports = {
+  handler: userHandler,
+}
