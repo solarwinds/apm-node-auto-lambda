71e4c74761ab21e083e764e6fa05ae38fedf44c2
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-09-21 05:33:00 -0700

    don't load agent if disabled by env

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-'use strict';
+const env = process.env;
+const apm = env.AO_TEST_LAMBDA_APM || 'appoptics-apm';
+const runtime = env.AO_TEST_LAMBDA_RUNTIME || '/var/runtime/UserFunction';
 
-const apm = process.env.AO_TEST_LAMBDA_APM || 'appoptics-apm';
-const runtime = process.env.AO_TEST_LAMBDA_RUNTIME || '/var/runtime/UserFunction';
-const ao = require(apm);
+// this duplicates the logic in the agent.
+const envSetting = env.APPOPTICS_ENABLED && env.APPOPTICS_ENABLED.toLowerCase() || 'true';
+const enabled = ~['true', 't', '1', 'yes', 'y', 'on'].indexOf(envSetting);
 
-const taskRoot = process.env.LAMBDA_TASK_ROOT || '';
-const handler = process.env.APPOPTICS_LAMBDA_HANDLER || '';
+const taskRoot = env.LAMBDA_TASK_ROOT || '';
+const handler = env.APPOPTICS_WRAP_LAMBDA_HANDLER || '';
 
-// use the lambda runtime's loading logic
-let load;
 let userHandler;
 try {
-  ({load} = require(runtime));
+  // use the lambda runtime's loader logic.
+  const {load} = require(runtime);
   userHandler = load(taskRoot, handler);
 } catch (e) {
-  ao.loggers.error(`failed to load ${handler}`, e);
+  // eslint-disable-next-line no-console
+  console.error(`failed to load ${handler}`);
 }
 
-if (ao.cfg.enabled) {
-  userHandler = ao.wrapLambdaHandler(userHandler);
+// only load our agent code if not disabled and the user handler
+// was loaded.
+if (enabled && userHandler) {
+  const ao = require(apm);
+
+  // if nothing caused the agent to be disabled then
+  // wrap the user's handler.
+  if (ao.cfg.enabled) {
+    userHandler = ao.wrapLambdaHandler(userHandler);
+    ao.loggers.debug(`wrapped ${taskRoot} ${handler}`);
+  }
 }
 
 module.exports = {
